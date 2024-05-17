85355e1d17c85f1dc2009a5ffa18cd486ac862a3
Thayna Santos <thaynasant.ana@hotmail.com>
2024-05-17 18:17:57 -0300

    feature: primeiro commit

---
+++ PFinal_v2/Migrations/20240517191813_MigrationDepartamento.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PFinal_v2.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationDepartamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamento",
+                columns: table => new
+                {
+                    DepartamentoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamento", x => x.DepartamentoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Departamento");
+        }
+    }
+}
---
+++ PFinal_v2/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace PFinal_v2.Models
+{
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public int DepartamentoId { get; set; }
+        public DateTime DataContratacao { get; set; }
+        public bool IsAdmin { get; set; }
+        public string? Senha { get; set; }
+
+    }
+}
